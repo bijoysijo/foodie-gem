1c40637bb968b1c54ca3c00ce7ffe9fff22fd2a3
Bijoy Sijo <bijoysijo21@gmail.com>
2020-05-13 15:47:27 +0530

    create test for food.rb

--- spec/foodie_spec.rb
+++ spec/foodie_spec.rb
@@ -1,9 +1,17 @@
+require "Foodie"
+
 RSpec.describe Foodie do
   it "has a version number" do
     expect(Foodie::VERSION).not_to be nil
   end
+end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+RSpec.describe Foodie::Food do
+  it "broccoli is gross" do
+    expect(Foodie::Food.portray("Broccoli")).to eql("Gross!")
+  end
+
+  it "anything else is delicious" do
+    expect(Foodie::Food.portray("Not Broccoli")).to eql("Delicious!")
   end
 end
